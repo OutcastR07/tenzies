edbd4a4d37830cec2af8e6ede888e2a9eb75ca40
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2022-12-28 14:51:17 +0600

    roll-button-number-randomizer

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,29 @@ import Die from "./Die"
 import { useState } from 'react'
 
 export default function App() {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push((Math.floor(Math.random() * 6)) + 1)
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
   return (
-    <main>
+    < main >
       <div className="dice-container">
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
+        {diceElements}
       </div>
+      <button className="roll-btn" onClick={rollDice}>Roll</button>
     </main >
   )
 }
